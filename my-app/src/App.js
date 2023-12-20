52e6c459496550e48c149ca209ed63f789e48ef3
Dan Morriss <danielmorriss1@gmail.com>
2023-12-20 23:34:46 +0000

    Add Content.js and PostItem.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import css from './App.module.css';
-import NavBarForm from './components/NavBarForm';
+import NavBarForm from './components/NavBarForm'
+import SideBar from './components/Sidebar'
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
+      <SideBar />
+      <Content />
     </div>
   );
 }
