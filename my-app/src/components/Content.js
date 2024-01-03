8ee809789b4b6ba6e0ed3ebe7aed556b1c37f0f5
Dan Morriss <danielmorriss1@gmail.com>
2024-01-03 18:01:08 +0000

    Add search function

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import css from './css/Content.module.css';
-import {savedPosts} from '../posts.json';
+import data from '../posts.json';
 import PostItem from './PostItem';
 import Loader from './Loader';
+
+const savedPosts = data.savedPosts;
 
 class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
 
     componentDidMount() {
-        setTimeout(() => {this.setState({isLoaded: true})}, 2500);
-    }    
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+                posts: savedPosts
+            })
+        }, 2000);
+    }
+    
+    handleChange = (event) => {
+        const name = event.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter(post => {
+            return post.name.toLowerCase().includes(name)
+        })
+        
+        this.setState({
+            posts: filteredPosts
+        })
+    }
 
     render() {
         return (
@@ -22,10 +41,22 @@ class Content extends Component {
 
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput" id="searchInput">Search:</label>
+                        <input
+                            type='search'
+                            id='searchInput'
+                            placeholder='By Author'
+                            name='searchInput'
+                            onChange={(event) => this.handleChange(event)}
+                        />
+                        <h4>posts found: {this.state.posts.length}</h4>
+                    </form>
                 </div>
 
                 <div className={css.SearchResults}>
-                    {this.state.isLoaded ? <PostItem savedPosts={savedPosts} />
+                    {this.state.isLoaded ?
+                    <PostItem savedPosts={this.state.posts} />
                     : <Loader />}
                 </div>
 
